7c3c663470c4f3330e5f0ef3566e2b65f29e3d42
Andrujus <justasandrusk@gmail.com>
2025-02-21 13:22:24 +0200

    sutvarkytas vektoriai.cpp failas

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ double apskaiciuotiMediana(std::vector<int> pazymiai) {
 }
 
 
-void Duom(Student& studentas) {
+void Duom(Student& studentas, std::vector<Student>& studentai) {
     std::string kitas_stud = "T";
     while(kitas_stud == "T" || kitas_stud == "t")
     {
@@ -34,7 +34,11 @@ void Duom(Student& studentas) {
     std::cin >> studentas.pavarde;
 
     std::cout << "Įveskite namų darbų pažymius (įveskite -1, kad baigtumėte): ";
+    
+    studentas.namuDarbai.clear();
+
     int pazymys;
+
     while (true) {
         std::cin >> pazymys;
         if (pazymys == -1) break;
@@ -43,6 +47,7 @@ void Duom(Student& studentas) {
 
     std::cout << "Įveskite egzamino rezultatą: ";
     std::cin >> studentas.egz;
+    studentai.push_back(studentas);
     std::string kitas;
     std::cout<<"Ar norite testi? (T - taip, N - ne): ";
     std::cin>>kitas;
@@ -51,50 +56,49 @@ void Duom(Student& studentas) {
     else{
         kitas_stud = "N";
     }
-    
     }
-    
-    
 }
 
-void Rez(const Student& studentas) {
-    double Galutinis = Vidurkis(studentas.namuDarbai) * 0.4 + studentas.egz * 0.6;
+void Rez(const std::vector<Student>& studentai, bool n_vid) {
     std::cout << std::fixed << std::setprecision(2);
-    std::cout << "Studentas: " << studentas.vardas << " " << studentas.pavarde << std::endl;
-    std::cout << "Galutinis: " << Galutinis << std::endl;
-}
-
-int main() {
-    Student studentas;
-    Duom(studentas);
-    std::string vid;
-    std::cout<<"V - vidurkis, M - mediana"<<std::endl;
-    std::cin>>vid;
-    if(vid == "V" || vid =="v")
+    if(n_vid)
     {
         std::cout << "-----------------------------------------------------------"<<std::endl;
         std::cout << "Vardas     Pavarde       Galutunis (vid.)"<<std::endl;
         std::cout << "-----------------------------------------------------------"<<std::endl;
-            double Galutinis = Vidurkis(studentas.namuDarbai) * 0.4 + studentas.egz * 0.6;
-        double Galetunis_med = apskaiciuotiMediana(studentas.namuDarbai);
-        std::cout << std::fixed << std::setprecision(2);
-    
-        std::cout<< studentas.vardas << "        " << studentas.pavarde << "       " << Galutinis;
     }
     else
     {
         std::cout << "-----------------------------------------------------------"<<std::endl;
         std::cout << "Vardas    Pavarde      Galutunis (med.)"<<std::endl;
         std::cout << "-----------------------------------------------------------"<<std::endl;
-        double Galutinis = Vidurkis(studentas.namuDarbai) * 0.4 + studentas.egz * 0.6;
-        double mediana = apskaiciuotiMediana(studentas.namuDarbai);
-        double Galutinis_med = 0.4*mediana+0.6*studentas.egz;
-        std::cout << std::fixed << std::setprecision(2);
     
-        std::cout<< studentas.vardas << "        " << studentas.pavarde << "       " << Galutinis_med;
     }
-    
+    for (const auto& studentas : studentai) {
+        double galutinis;
+        if (n_vid) {
+            
+            galutinis = Vidurkis(studentas.namuDarbai) * 0.4 + studentas.egz * 0.6;
+        } else {
+            
+            double mediana = apskaiciuotiMediana(studentas.namuDarbai);
+        }
+        
+        std::cout << studentas.vardas << "     " << studentas.pavarde << "       " << galutinis << std::endl;
+    }
+}
 
-    
+int main() {
+    Student studentas;
+    std::vector<Student> studentai;
+    Duom(studentas, studentai);
+    std::string vid;
+    std::cout<<"V - vidurkis, M - mediana"<<std::endl;
+    std::cin>>vid;
+    if (vid == "V" || vid == "v") {
+        Rez(studentai, true);
+    } else {
+        Rez(studentai, false);
+    }
     return 0;
-};
+}
